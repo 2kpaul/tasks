55d71b5a488f60921fc7f5a7f9105e5c140b1267
Paul Maxineanu <paul.maxineanu@gmail.com>
2021-11-03 12:21:59 +0200

    create vue components

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,18 @@
 <template>
     <header>
-        <h1>Task Tracker</h1>
+        <h1>{{ title }}</h1>
+        <Button text="Add Task" color="green"></Button>
     </header>
 </template>
 
 <script>
+import Button from './Button'
 export default {
-    name: 'Header'
+    name: 'Header',
+    props: ['title'],
+    components: {
+        Button
+    }
 }
 </script>
 
